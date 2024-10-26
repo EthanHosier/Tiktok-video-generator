8ee6a0c60d7224ee0d2663434d6beeb0872ffad8
Ethan Hosier <ethanjhosier@gmail.com>
2024-10-27 00:40:55 +0100

    feat: add audio and video download urls

--- main.go
+++ main.go
@@ -1,10 +1,20 @@
 package main
 
-import "github.com/ethanhosier/clips/youtube"
+import (
+	"fmt"
+
+	"github.com/ethanhosier/clips/youtube"
+)
 
 const id = "Tb8WYOB89i4"
 
 func main() {
 	yt := youtube.NewYoutubeClient()
-	yt.VideoForId(id)
+	vid, err := yt.VideoForId(id)
+	if err != nil {
+		fmt.Printf("error getting video: %v\n", err)
+		return
+	}
+
+	fmt.Printf("vid: %+v\n", vid)
 }
--- youtube/youtube.go
+++ youtube/youtube.go
@@ -2,13 +2,8 @@ package youtube
 
 import (
 	"fmt"
-	"log"
-	"net/http"
-	"net/url"
 	"time"
 
-	"github.com/go-rod/rod"
-	"github.com/go-rod/rod/lib/launcher"
 	"github.com/kkdai/youtube/v2"
 )
 
@@ -19,85 +14,52 @@ type YoutubeHandler interface {
 type YoutubeClient struct {
 }
 
+type Video struct {
+	ID              string
+	Title           string
+	Description     string
+	Author          string
+	ChannelID       string
+	ChannelHandle   string
+	Views           int
+	Duration        time.Duration
+	PublishDate     time.Time
+	DASHManifestURL string
+	HLSManifestURL  string
+	CaptionTrack    string
+	AudioURL        string
+	VideoURL        string
+}
+
 func NewYoutubeClient() *YoutubeClient {
 	return &YoutubeClient{}
 }
 
-func (y *YoutubeClient) CaptionsForId(id string) {
-	serverErrCh := make(chan error)
-	go startServer(serverErrCh, id)
-
-	requestUrlCh := make(chan *url.URL)
-	go simulateBrowserForUrl(requestUrlCh)
-
-	// Handle any server errors from the serverErrCh channel
-	select {
-	case err := <-serverErrCh:
-		log.Fatalf("Server error %v", err)
-	case requestUrl := <-requestUrlCh:
-		fmt.Println("Received request URL:", requestUrl)
-	case <-time.After(10 * time.Second):
-		log.Fatal("Timeout")
-	}
-}
-
-func (y *YoutubeClient) VideoForId(id string) {
+func (y *YoutubeClient) VideoForId(id string) (*Video, error) {
 	url := fmt.Sprintf("https://www.youtube.com/watch?v=%s", id)
 	client := youtube.Client{}
 	video, err := client.GetVideo(url)
 	if err != nil {
-		log.Fatalf("Error getting video: %v", err)
+		return nil, fmt.Errorf("error getting video: %v\n\n", err)
 	}
 
-	fmt.Printf("%+v", video)
-}
+	hdFormat := video.Formats[0]
+	audioFormat := video.Formats.Itag(140)[0]
 
-func handler(id string) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		html := fmt.Sprintf(`<!DOCTYPE html>
-	<html lang="en">
-	<head>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>Document</title>
-	</head>
-	<body>
-		<iframe width="560" height="315" src="https://www.youtube.com/embed/%s?si=jGSgSFl5DP3Rxyin&cc_load_policy=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-	</body>
-	</html>`, id)
-		fmt.Fprint(w, html)
-	}
-}
-
-func startServer(errCh chan error, id string) {
-	http.HandleFunc("/", handler(id))
-	fmt.Println("Starting server on :8080...")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		errCh <- err
-	}
-}
-
-func simulateBrowserForUrl(requestUrlCh chan *url.URL) {
-	url := launcher.New().Headless(true).MustLaunch()
-	browser := rod.New().ControlURL(url).MustConnect()
-	defer browser.MustClose()
-
-	router := browser.HijackRequests()
-	defer router.MustStop()
-
-	router.MustAdd("*youtube.com/api/timedtext*", func(ctx *rod.Hijack) {
-		requestUrlCh <- ctx.Request.URL()
-		ctx.ContinueRequest(nil)
-	})
-
-	go router.Run()
-
-	page := browser.MustPage("http://localhost:8080")
-	page.MustWaitLoad()
-	page.MustWaitRequestIdle()
-
-	time.Sleep(5 * time.Second)
-	page.Mouse.MustMoveTo(300, 300).MustClick("left")
-	fmt.Println("Clicked on the video")
-	time.Sleep(5 * time.Second)
+	return &Video{
+		ID:              video.ID,
+		Title:           video.Title,
+		Description:     video.Description,
+		Author:          video.Author,
+		ChannelID:       video.ChannelID,
+		ChannelHandle:   video.ChannelHandle,
+		Views:           video.Views,
+		Duration:        video.Duration,
+		PublishDate:     video.PublishDate,
+		DASHManifestURL: video.DASHManifestURL,
+		HLSManifestURL:  video.HLSManifestURL,
+		CaptionTrack:    video.CaptionTracks[0].BaseURL,
+		AudioURL:        audioFormat.URL,
+		VideoURL:        hdFormat.URL,
+	}, nil
 }
